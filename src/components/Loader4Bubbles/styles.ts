4c44ddbe6c1bf58c7cbe784351adeb91103acbe5
Pedro Florencio <pedroflorencio@alu.ufc.br>
2022-02-17 21:18:55 -0300

    add loader with setTimeOut

---
+++ src/components/Loader4Bubbles/styles.ts
@@ -0,0 +1,49 @@
+import styled, { keyframes } from "styled-components";
+
+/* Loader by Frank Matos */
+
+const rotate = keyframes`
+to {
+  transform: rotate(2turn);
+}
+`;
+
+export const LoaderContent = styled.div`
+  position: absolute;
+  top: 30%;
+  left:48%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 50px;
+  height: 50px;
+
+  animation: ${rotate} 2s infinite;
+`;
+
+export const Circles = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 40%;
+  margin: 2px;
+
+  &:nth-child(2) {
+    flex-direction: row-reverse;
+  }
+`;
+
+export const Circle = styled.div`
+  display: flex;
+  width: 15px;
+  height: 15px;
+  background: var(--THEME_COLOR_007);
+  margin: 2px;
+  border-radius: 50%;
+
+  &:nth-child(1) {
+    background: var(--THEME_COLOR_001);
+    animation-delay: 0.5s;
+  }
+`;
--- src/templates/Default/index.tsx
+++ src/templates/Default/index.tsx
@@ -1,22 +1,30 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ItemsArea from "../../components/ListArea";
 import SearchBar from "../../components/Header";
 import GlobalStyles from "../../global/styles/GlobalStyles";
 import Footer from "../../components/Footer"
-
+import Loader from "../../components/Loader4Bubbles"
 import { Grid } from "./style";
 
 const Default: React.FC = () => {
-  const [searchPokemons, setSearchPokemons] = useState('')
+const [searchPokemons, setSearchPokemons] = useState('')
+const [loading, setLoading] = useState(true)
+
+ useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    },1800);
+  })
 
   return (
     <>
       <GlobalStyles />
+ 
+
       <Grid>
+        <Loader show={loading} />
         <SearchBar searchPokemons={searchPokemons} setSearchPokemons={setSearchPokemons} />
-
         <ItemsArea searchPokemons={searchPokemons} />
-
         <Footer />
       </Grid>
     </>
